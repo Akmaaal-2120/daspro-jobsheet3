71bf6037ac17b034f05842ac161fac5838344639
Akmaaal-2121 <muhamadmiftahul21@gmail.com>
2024-09-13 13:12:34 +0700

    Siakad15

---
+++ siakad.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class siakad{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama, nim;
+        String kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir, nilaiUjianAkhir;
+
+        System.out.println("Masukkan nama : ");
+        nama = input.nextLine();
+        System.out.println("Masukkan nim : ");
+        nim = input.nextLine();
+        System.out.println("Masukkan kelas : ");
+        kelas = input.nextLine();
+        System.out.println("Masukkan NoAbsen : ");
+        absen = input.nextByte();
+
+        System.out.println("Masukkan nilai kuis : ");
+        nilaiKuis = input.nextDouble();
+        System.out.println("Masukkan nilai ujian tengah : ");
+        nilaiUjian = input.nextDouble();
+        System.out.println("Masukkan nilai tugas : ");
+        nilaiTugas = input.nextDouble();
+        System.out.println("Masukkan nilai ujian akhir : ");
+        nilaiUjianAkhir = input.nextDouble();
+
+        nilaiAkhir = ((nilaiKuis * 0.2) + (nilaiTugas * 0.15) + (nilaiUjian * 0.3) + (nilaiUjianAkhir * 0.35));
+
+        System.out.println("Mahasiswa dengan Nama : " + nama + " (NIM : " + nim + ")" + " kelas : " + kelas + " No Absen : " + absen );
+        System.out.println("Nilai Akhir : " + nilaiAkhir);
+
+    }
+}
